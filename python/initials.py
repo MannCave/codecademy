e05f180962e16e24b831c9a0a1d0b6cca31cb89e
Houston Mannis <houstonmannis@gmail.com>
2025-09-03 12:04:31 -0500

    Create initials.py

---
+++ python/initials.py
@@ -0,0 +1,13 @@
+# My name is Houston
+# I'm trying to grow and learn programing languages
+lines = [
+  "H   H   M   M",
+  "H   H   MM MM",
+  "H   H   MM MM",
+  "HHHHH   M M M",
+  "H   H   M   M",
+  "H   H   M   M",
+  "H   H   M   M"
+]
+for line in lines:
+    print(line)
